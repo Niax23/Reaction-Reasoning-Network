cdb2cccf0eb8641d87126a092c70146b0ea22fc4
zengkaipeng <zkp0819@126.com>
2024-09-15 13:04:02 +0800

    modifing ddp

--- ddp_training.py
+++ ddp_training.py
@@ -8,49 +8,112 @@ from utils.data_utils import (
     convert_log_into_label, data_eval_trans
 )
 
+from training import warmup_lr_scheduler, calc_trans_loss
 
-def warmup_lr_scheduler(optimizer, warmup_iters, warmup_factor):
-    def f(x):
-        if x >= warmup_iters:
-            return 1
-        alpha = float(x) / warmup_iters
-        return warmup_factor * (1 - alpha) + alpha
-
-    return torch.optim.lr_scheduler.LambdaLR(optimizer, f)
+import torch.distributed as torch_dist
+from enum import Enum
 
 
-def calc_trans_loss(trans_pred, trans_lb, ignore_index, lbsm=0.0):
-    batch_size, maxl, num_c = trans_pred.shape
-    trans_pred = trans_pred.reshape(-1, num_c)
-    trans_lb = trans_lb.reshape(-1)
-
-    losses = cross_entropy(
-        trans_pred, trans_lb, reduction='none',
-        ignore_index=ignore_index, label_smoothing=lbsm
-    )
-    losses = losses.reshape(batch_size, maxl)
-    loss = torch.mean(torch.sum(losses, dim=-1))
-    return loss
+class Summary(Enum):
+    NONE, SUM, AVERAGE, COUNT = 0, 1, 2, 3
 
 
-def train_uspto_condition(loader, model, optimizer, device, warmup=False):
-    model, los_cur = model.train(), []
+class MetricCollector(object):
+    def __init__(self, name, type_fmt=':f', summary_type=Summary.AVERAGE):
+        super(MetricCollector, self).__init__()
+        self.name, self.type_fmt = name, type_fmt
+        self.summary_type = summary_type
+        self.reset()
+
+    def reset(self):
+        self.val, self.sum, self.cnt, self.avg = [0] * 4
+
+    def update(self, val, num=1):
+        self.val = val
+        self.sum += val
+        self.cnt += num
+        self.avg = self.sum / self.cnt
+
+    def all_reduce(self, device):
+        infos = torch.FloatTensor([self.sum, self.cnt]).to(device)
+        torch_dist.all_reduce(infos, torch_dist.ReduceOp.SUM)
+        self.sum, self.cnt = infos.tolist()
+        self.avg = self.sum / self.cnt
+
+    def __str__(self):
+        return ''.join([
+            '{name}: {val', self.type_fmt, '} avg: {avg', self.type_fmt, '}'
+        ]).format(**self.__dict__)
+
+    def summary(self):
+        if self.summary_type is Summary.NONE:
+            fmtstr = ''
+        elif self.summary_type is Summary.AVERAGE:
+            fmtstr = '{name} {avg:.3f}'
+        elif self.summary_type is Summary.SUM:
+            fmtstr = '{name} {sum:.3f}'
+        elif self.summary_type is Summary.COUNT:
+            fmtstr = '{name} {cnt:.3f}'
+        else:
+            raise ValueError(f'Invaild summary type {self.summary_type} found')
+
+        return fmtstr.format(**self.__dict__)
+
+    def get_value(self):
+        if self.summary_type is Summary.AVERAGE:
+            return self.avg
+        elif self.summary_type is Summary.SUM:
+            return self.sum
+        elif self.summary_type is Summary.COUNT:
+            return self.cnt
+        else:
+            raise ValueError(
+                f'Invaild summary type {self.summary_type} '
+                'for get_value()'
+            )
+
+
+class MetricManager(object):
+    def __init__(self, metrics):
+        super(MetricManager, self).__init__()
+        self.metrics = metrics
+
+    def all_reduct(self, device):
+        for idx in range(len(self.metrics)):
+            self.metrics[idx].all_reduce(device)
+
+    def summary_all(self, split_string='  '):
+        return split_string.join(x.summary() for x in self.metrics)
+
+    def get_all_value_dict(self):
+        return {x.name: x.get_value() for x in self.metrics}
+
+
+def ddp_train_uspto_condition(
+    loader, model, optimizer, device, warmup=False, verbose=False
+):
+    model = model.train()
+    loss_cur = MetricCollector('loss', type_fmt=':.3f')
+    manager = MetricManager([loss_cur])
     if warmup:
         warmup_iters = len(loader) - 1
         warmup_sher = warmup_lr_scheduler(optimizer, warmup_iters, 5e-2)
 
-    for data in tqdm(loader):
+    iterx = tqdm(loader) if verbose else loader
+
+    for data in iterx:
         mol_graphs, edge_index, edge_types, mol_mask, reaction_mask, \
             req_ids, labels, label_types = data
 
-        mol_graphs = mol_graphs.to(device)
-        edge_index = edge_index.to(device)
-        mol_mask = mol_mask.to(device)
-        reaction_mask = reaction_mask.to(device)
-        labels = labels.to(device)
-        label_types = label_types.to(device)
+        mol_graphs = mol_graphs.to(device, non_blocking=True)
+        edge_index = edge_index.to(device, non_blocking=True)
+        mol_mask = mol_mask.to(device, non_blocking=True)
+        reaction_mask = reaction_mask.to(device, non_blocking=True)
+        labels = labels.to(device, non_blocking=True)
+        label_types = label_types.to(device, non_blocking=True)
 
-        sub_mask = generate_square_subsequent_mask(5, device)
+        sub_mask = generate_square_subsequent_mask(5, 'cpu')
+        sub_mask = sub_mask.to(device, non_blocking=True)
 
         res = model(
             molecules=mol_graphs, molecule_mask=mol_mask, required_ids=req_ids,
@@ -63,14 +126,17 @@ def train_uspto_condition(loader, model, optimizer, device, warmup=False):
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-        los_cur.append(loss.item())
+        loss_cur.update(loss.item())
         if warmup:
             warmup_sher.step()
 
-    return np.mean(los_cur)
+        if verbose:
+            iterx.set_postfix_str(manager.summary_all())
+
+    return manager
 
 
-def eval_uspto_condition(loader, model, device):
+def ddp_eval_uspto_condition(loader, model, device):
     model, accs, gt = model.eval(), [], []
     for data in tqdm(loader):
         mol_graphs, edge_index, edge_types, mol_mask, reaction_mask, \
