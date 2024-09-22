ee2a44ce369454395000ae599a2cae5c0a188d86
zengkaipeng <zkp0819@126.com>
2024-09-22 15:11:01 +0800

    addam

--- pretrain/pretrain_gnn_utils.py
+++ pretrain/pretrain_gnn_utils.py
@@ -97,3 +97,67 @@ def smiles2graph(smiles_string):
     graph['num_nodes'] = len(x)
 
     return graph
+
+
+def smiles2graph_with_am(smiles_string):
+    """
+    Converts SMILES string to graph Data object
+    :input: SMILES string (str)
+    :return: graph object
+    """
+
+    mol = Chem.MolFromSmiles(smiles_string)
+    if len(mol.GetAtoms()) > 0:
+        max_amap = max([atom.GetAtomMapNum() for atom in mol.GetAtoms()])
+        for atom in mol.GetAtoms():
+            if atom.GetAtomMapNum() == 0:
+                atom.SetAtomMapNum(max_amap + 1)
+                max_amap = max_amap + 1
+
+        amap_idx = {
+            atom.GetAtomMapNum(): atom.GetIdx()
+            for atom in mol.GetAtoms()
+        }
+
+    # atoms
+    atom_features_list = []
+    for atom in mol.GetAtoms():
+        atom_features_list.append(atom_to_feature(atom))
+    x = np.array(atom_features_list, dtype=np.int64)
+
+    # bonds
+    num_bond_features = 2
+    if len(mol.GetBonds()) > 0:  # mol has bonds
+        edges_list = []
+        edge_features_list = []
+        for bond in mol.GetBonds():
+            i = bond.GetBeginAtomIdx()
+            j = bond.GetEndAtomIdx()
+
+            edge_feature = bond_to_feature(bond)
+
+            # add edges in both directions
+            edges_list.append((i, j))
+            edge_features_list.append(edge_feature)
+            edges_list.append((j, i))
+            edge_features_list.append(edge_feature)
+
+        # data.edge_index: Graph connectivity in
+        # COO format with shape [2, num_edges]
+        edge_index = np.array(edges_list, dtype=np.int64).T
+
+        # data.edge_attr: Edge feature matrix with
+        # shape [num_edges, num_edge_features]
+        edge_attr = np.array(edge_features_list, dtype=np.int64)
+
+    else:  # mol has no bonds
+        edge_index = np.empty((2, 0), dtype=np.int64)
+        edge_attr = np.empty((0, num_bond_features), dtype=np.int64)
+
+    graph = dict()
+    graph['edge_index'] = edge_index
+    graph['edge_feat'] = edge_attr
+    graph['node_feat'] = x
+    graph['num_nodes'] = len(x)
+
+    return graph, amap_idx
